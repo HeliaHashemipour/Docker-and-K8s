84a4f574e806e9ef5732126a3bc2be84da51c79a
HeliaHashemipour <heliahashemipour2@gmail.com>
2022-12-19 20:35:32 +0330

    Add : Section2

---
+++ 2.CoinAPI/app.py
@@ -0,0 +1,34 @@
+import flask
+from redis import Redis
+import requests
+import os
+
+
+app = flask.Flask(__name__)
+redis_db = Redis(host='redis', port=6379, db=0)
+
+CACHE_EXPIRE_TIME = int(os.getenv('CACHE_EXPIRE_TIME'))
+CACHE_PORT = int(os.getenv('CACHE_PORT'))
+COIN_NAME = os.getenv('COIN_NAME')
+
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
+
+@app.route('/')
+def get_coin_price():
+    coin_name = COIN_NAME
+    price = redis_db.get(coin_name)
+    if price is None:
+        price = requests.get(
+            f'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids={coin_name}&order=market_cap_desc&per_page=100&page=1&sparkline=false').json()[0]['current_price']
+
+        redis_db.set(coin_name, price, ex=CACHE_EXPIRE_TIME)
+
+    return flask.jsonify({"name": coin_name, "price": price})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
